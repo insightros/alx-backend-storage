56a1174c6a1d8cf966454a04a7ec38d7f1c8f9f2
xeno <xeno@xeno.0.2.15>
2024-04-25 15:38:40 +0100

    mysql advanced

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,6 @@
 -- lists all bands with Glam rock as their main style
-SELECT 
-    band_name,
-    TIMESTAMPDIFF(YEAR, formed, IFNULL(split, '2022-01-01')) AS lifespan
-FROM 
-    bands
-WHERE 
-    style = 'Glam rock'
-ORDER BY 
-    lifespan DESC;
+
+SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
